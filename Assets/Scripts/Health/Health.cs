a840ad0e45478d7ed0879cdf3fc1c03a75220d49
lipskii198 <albertovisco7@gmail.com>
2022-10-15 19:22:27 +0200

    invincibility
    added invincibility after gettin hit, new layer for player and enemy, player flashes red when he's invincible

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [Header("Health")]
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; } //can get it anywhere but can only modify it in this script
 
+    [Header("IFrame")]//deals with invincibility frame 
+    [SerializeField] private float iFrameDuration;
+    [SerializeField] private float numberOfFlashes;
+    private SpriteRenderer spriteRend; 
     void Awake()
     {
         currentHealth = startingHealth;
+        spriteRend = GetComponent<SpriteRenderer>(); 
 
     }
 
@@ -28,6 +34,8 @@ public class Health : MonoBehaviour
         if (currentHealth > 0)
         {
             //take damage 
+            //becomes invincible and flashes red 
+            StartCoroutine(Invulnerability());
         }
         else
         {
@@ -37,5 +45,18 @@ public class Health : MonoBehaviour
     public void AddHealth(float _value)
     {
         currentHealth = Mathf.Clamp(currentHealth +  _value, 0, startingHealth);
+    }
+    private IEnumerator Invulnerability()
+    {
+        Physics2D.IgnoreLayerCollision(10, 11, true); // layer 10(player) ignores things from layer 11(enemy)
+        //wait time before turning collision back on
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0,0.5f); //turn player sprite red, 0.5 is to make it a little bit transparent  
+            yield return new WaitForSeconds(iFrameDuration/(numberOfFlashes*2));//waits half the time of invincibility/nflash 
+            spriteRend.color = Color.white; // turn player back white
+            yield return new WaitForSeconds(iFrameDuration / (numberOfFlashes * 2));//waits  
+        }
+        Physics2D.IgnoreLayerCollision(10, 11, false);
     }
 }
