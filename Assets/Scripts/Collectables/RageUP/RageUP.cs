f0352d1a51d2f5688b506276f20a065a1d8a78e2
lipskii198 <albertovisco7@gmail.com>
2022-11-21 10:07:26 +0100

    added rageUP script

---
+++ Assets/Scripts/Collectables/RageUP/RageUP.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RageUP : MonoBehaviour
+{
+    [SerializeField] private float rageDuration;
+    [SerializeField] private float ShootingSpeedReduce;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            //if object touches player it starts rage time 
+            //collision.GetComponent<PlayerAttack>().RageUP(rageDuration, ShootingSpeedReduce);
+            gameObject.SetActive(false);
+        }
+    }
+}
