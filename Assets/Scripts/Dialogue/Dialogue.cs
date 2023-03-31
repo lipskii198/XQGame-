92a4d448d76cfc8c8ab890bc808e43bb350095ce
Omar Farag <omarfarag74@gmail.com>
2023-03-31 03:02:21 -0400

    Add audio playing, need to fix text queue

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -9,7 +9,13 @@ public class Dialogue
 {
     public string name { get; set; }
     public string sprite { get; set; }
-    public string audio { get; set; }
+    public Audio audio { get; set; }
     public List<string> sentences { get; set; }
+}
+
+public class Audio
+{
+    public string file { get; set; }
+    public float volume { get; set; }
 }
 
--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -7,48 +7,88 @@ public class DialogueManager : MonoBehaviour
 
     Queue<string> sentences;
     Queue<Dialogue> dialogues;
+    AudioClip audioClip;
+    Vector3 audioPosition;
+    bool buttonPressed;
+    bool sequenceStarted;
     // Start is called before the first frame update
     void Start()
-    {   
+    {
         dialogues = new Queue<Dialogue>();
         sentences = new Queue<string>();
+        audioPosition = new Vector3(0, 0, 0);
+        sequenceStarted = false;
+
+    }
+    void Update()
+    {
+      
+            // Pause the loop and wait for a button to be pressed
+            if (Input.GetKeyDown(KeyCode.Space) && sequenceStarted)
+            {
+                DisplayNextSentence();
+            }
+        
     }
 
-    public void EnqueueDialogue(Dialogue d) {
+    public void EnqueueDialogue(Dialogue d)
+    {
         dialogues.Enqueue(d);
     }
 
-    public void StartSequence() {
-        while (dialogues.Count > 0) {
+    public void StartSequence()
+    {   
+        sequenceStarted = true;
+        while (dialogues.Count > 0)
+        {
             Dialogue d = dialogues.Dequeue();
             StartDialogue(d);
         }
     }
 
-    public void StartDialogue(Dialogue d) {
-        Debug.Log("starting dialogue" + d.name);
+    public void StartDialogue(Dialogue d)
+    {
+        Debug.Log("starting dialogue " + d.name);
 
         sentences.Clear();
+        //Play audio
+        if (d.audio.file != "")
+        {
+            // Load the AudioClip from a file
+            audioClip = Resources.Load<AudioClip>(d.audio.file);
+            Debug.Log(d.audio);
+            // Play the audio clip
+            if (audioClip != null) AudioSource.PlayClipAtPoint(audioClip, audioPosition, d.audio.volume);
+        }
+        //Render character sprite
+        //Render dialogue box
 
         foreach (string sentence in d.sentences)
         {
             sentences.Enqueue(sentence);
         }
-
         DisplayNextSentence();
+        
+        
     }
 
     //Called whenever the player hits the next button in the dialogue box
-    public void DisplayNextSentence() {
-        if (sentences.Count == 0) {
+    public void DisplayNextSentence()
+    {   
+        if (sentences.Count != 0) {
+            string sentence = sentences.Dequeue();
+            Debug.Log(sentence);
+        } else {
             EndDialogue();
             return;
         }
-        string sentence = sentences.Dequeue();
-        Debug.Log(sentence);
+        
     }
     //Probably need to do some garbage collection, will look into later
-    void EndDialogue() {
+    void EndDialogue()
+    {
+        sequenceStarted = false;
+        //Remove character sprite
         Debug.Log("ending dialogue");
     }
 }
