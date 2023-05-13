447be5c691631e51d35688fb412f159395474f76
Omar Farag <omarfarag74@gmail.com>
2023-05-13 18:04:34 -0400

    fixed build target i think

--- Assets/_game/Scripts/Utility/MyEditorScript.cs
+++ Assets/_game/Scripts/Utility/MyEditorScript.cs
@@ -5,7 +5,7 @@ class MyEditorScript
      {
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
         buildPlayerOptions.locationPathName = "build/Windows";
-        buildPlayerOptions.buildTarget = buildTarget.StandaloneWindows64;
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
         buildPlayerOptions.scenes = new[] { "Assets/_game/Scenes/MainMenu.unity", "Assets/_game/Scenes/AntsLevel.unity", "Assets/_game/Scenes/BossTest.unity", "Assets/_game/Scenes/Intro.unity"};
         BuildPipeline.BuildPlayer(buildPlayerOptions);
      }
