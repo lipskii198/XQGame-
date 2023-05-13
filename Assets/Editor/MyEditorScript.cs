e2201b368eb21e70572b2c24c10300842877eed6
Omar Farag <omarfarag74@gmail.com>
2023-05-13 18:25:40 -0400

    add exe extension

--- Assets/Editor/MyEditorScript.cs
+++ Assets/Editor/MyEditorScript.cs
@@ -4,7 +4,7 @@ class MyEditorScript
      static void PerformBuild ()
      {
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-        buildPlayerOptions.locationPathName = "build/Windows";
+        buildPlayerOptions.locationPathName = "build/Windows/Game.exe";
         buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
         buildPlayerOptions.scenes = new[] { "Assets/_game/Scenes/MainMenu.unity", "Assets/_game/Scenes/AntsLevel.unity", "Assets/_game/Scenes/BossTest.unity", "Assets/_game/Scenes/Intro.unity"};
         BuildPipeline.BuildPlayer(buildPlayerOptions);
