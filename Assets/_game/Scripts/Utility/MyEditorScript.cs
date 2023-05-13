9d939d9181cccc01fe0fe83af8ca9cb0bb66b4ac
Omar Farag <omarfarag74@gmail.com>
2023-05-13 17:47:26 -0400

    Added location path name

--- Assets/_game/Scripts/Utility/MyEditorScript.cs
+++ Assets/_game/Scripts/Utility/MyEditorScript.cs
@@ -4,6 +4,7 @@ class MyEditorScript
      static void PerformBuild ()
      {
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.locationPathName = "build/Windows";
         buildPlayerOptions.scenes = new[] { "Assets/_game/Scenes/MainMenu.unity", "Assets/_game/Scenes/AntsLevel.unity", "Assets/_game/Scenes/BossTest.unity", "Assets/_game/Scenes/Intro.unity"};
         BuildPipeline.BuildPlayer(buildPlayerOptions);
      }
