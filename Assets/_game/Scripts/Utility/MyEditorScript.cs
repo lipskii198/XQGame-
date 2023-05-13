ccd2e9f328b906ff3367c60edfd46bcf943fee0b
Omar Farag <omarfarag74@gmail.com>
2023-05-13 17:34:56 -0400

    Added build script

---
+++ Assets/_game/Scripts/Utility/MyEditorScript.cs
@@ -0,0 +1,10 @@
+using UnityEditor;
+class MyEditorScript
+{
+     static void PerformBuild ()
+     {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/_game/Scenes/MainMenu.unity", "Assets/_game/Scenes/AntsLevel.unity", "Assets/_game/Scenes/BossTest.unity", "Assets/_game/Scenes/Intro.unity"};
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+     }
+}
